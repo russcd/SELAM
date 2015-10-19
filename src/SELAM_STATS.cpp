640ff105033bc2a16633986d4e17fdadb8ae92e9
russcd <russcd@gmail.com>
2015-10-19 11:51:56 -0700

    stats_fix

--- src/SELAM_STATS.cpp
+++ src/SELAM_STATS.cpp
@@ -3,39 +3,104 @@
 
 /// VERSION 0.03
 
-
 #include <string>
 #include <iostream>
-#include <time.h> 
 #include <vector>
 #include <sstream>
 #include <fstream>
-#include <algorithm>
 #include <math.h>
+#include <stdlib.h>
 #include <map>
-#include <utility>
-#include <ctype.h>
-#include <boost/math/distributions/hypergeometric.hpp>
 
-using namespace boost::math;
-using namespace std;
+using namespace std ;
 
-/// fisher's exact test
-double fisher_test(unsigned a, unsigned b, unsigned c, unsigned d) {
-    unsigned N = a + b + c + d;
-    unsigned r = a + c;
-    unsigned n = c + d;
-    unsigned max_for_k = min(r, n);
-    unsigned min_for_k = (unsigned)max(0, int(r + n - N));
-    hypergeometric_distribution<> hgd(r, n, N);
-    double cutoff = pdf(hgd, c);
-    double tmp_p = 0.0;
-    for(int k = min_for_k;k < max_for_k + 1;k++) {
-        double p = pdf(hgd, k);
-        if(p <= cutoff) tmp_p += p;
-    }
-    return tmp_p;
+// log\binom{n}{k}
+static double lbinom(int n, int k)
+{
+    if (k == 0 || n == k) return 0;
+    return lgamma(n+1) - lgamma(k+1) - lgamma(n-k+1);
 }
+
+// n11  n12  | n1_
+// n21  n22  | n2_
+//-----------+----
+// n_1  n_2  | n
+
+// hypergeometric distribution
+static double hypergeo(int n11, int n1_, int n_1, int n)
+{
+    return exp(lbinom(n1_, n11) + lbinom(n-n1_, n_1-n11) - lbinom(n, n_1));
+}
+
+typedef struct {
+    int n11, n1_, n_1, n;
+    double p;
+} hgacc_t;
+
+// incremental version of hypergenometric distribution
+static double hypergeo_acc(int n11, int n1_, int n_1, int n, hgacc_t *aux) {
+    if (n1_ || n_1 || n) {
+        aux->n11 = n11; aux->n1_ = n1_; aux->n_1 = n_1; aux->n = n;
+    } else { // then only n11 changed; the rest fixed
+        if (n11%11 && n11 + aux->n - aux->n1_ - aux->n_1) {
+            if (n11 == aux->n11 + 1) { // incremental
+                aux->p *= (double)(aux->n1_ - aux->n11) / n11
+                * (aux->n_1 - aux->n11) / (n11 + aux->n - aux->n1_ - aux->n_1);
+                aux->n11 = n11;
+                return aux->p;
+            }
+            if (n11 == aux->n11 - 1) { // incremental
+                aux->p *= (double)aux->n11 / (aux->n1_ - n11)
+                * (aux->n11 + aux->n - aux->n1_ - aux->n_1) / (aux->n_1 - n11);
+                aux->n11 = n11;
+                return aux->p;
+            }
+        }
+        aux->n11 = n11;
+    }
+    aux->p = hypergeo(aux->n11, aux->n1_, aux->n_1, aux->n);
+    return aux->p;
+}
+
+double kt_fisher_exact(int n11, int n12, int n21, int n22, double &_left, double &_right, double &two) {
+    int i, j, max, min;
+    double p, q, left, right;
+    hgacc_t aux;
+    int n1_, n_1, n;
+    n1_ = n11 + n12; n_1 = n11 + n21; n = n11 + n12 + n21 + n22; // calculate n1_, n_1 and n
+    max = (n_1 < n1_) ? n_1 : n1_; // max n11, for right tail
+    min = n1_ + n_1 - n;
+    if (min < 0) min = 0; // min n11, for left tail
+    two = 1 ;
+    _left = _right = 1 ;
+    if (min == max) return 1.; // no need to do test
+    q = hypergeo_acc(n11, n1_, n_1, n, &aux); // the probability of the current table
+    // left tail
+    p = hypergeo_acc(min, 0, 0, 0, &aux);
+    for (left = 0., i = min + 1; p < 0.99999999 * q; ++i) // loop until underflow
+        left += p, p = hypergeo_acc(i, 0, 0, 0, &aux);
+    --i;
+    if (p < 1.00000001 * q) left += p;
+    else --i;
+
+    // right tail
+    p = hypergeo_acc(max, 0, 0, 0, &aux);
+    for (right = 0., j = max - 1; p < 0.99999999 * q; --j) // loop until underflow
+        right += p, p = hypergeo_acc(j, 0, 0, 0, &aux);
+    ++j;
+    if (p < 1.00000001 * q) right += p;
+    else ++j;
+    // two-tail
+    two = left + right;
+    cerr << two << endl ;
+    if (two > 1.) two = 1.;
+    // adjust left and right
+    if (abs(i - n11) < abs(j - n11)) right = 1. - left + q;
+    else left = 1.0 - right + q;
+    _left = left; _right = right;
+    return q;
+}
+
 
 class ancestry_tract {
 public:
@@ -397,9 +462,9 @@ void tract_info::compute_ld_stats ( double window_size, bool fet ) {
                         }
                     }
                     
-                    double p = -1 ;
+                    double p, right, left ;
                     if ( fet ) {
-                        p = fisher_test( ldm.at(0).at(0), ldm.at(0).at(1), ldm.at(1).at(0), ldm.at(1).at(1) ) ;
+                        double q = kt_fisher_exact( ldm.at(0).at(0), ldm.at(0).at(1), ldm.at(1).at(0), ldm.at(1).at(1), left, right, p ) ;
                     }
                     
                     for ( int i = 0 ; i < ldm.size() ; i ++ ) {
@@ -473,9 +538,9 @@ void tract_info::compute_gametic_linkage_stats ( double window_size, int parent,
                         ldm[sample.at(i).chromosomes.at(c1->first*2+parent).at(iterator1.at(i)).type][sample.at(i).chromosomes.at(c2->first*2+parent).at(iterator2.at(i)).type] ++ ;
                     }
                     
-                    double p = -1 ;
+                    double p, right, left ;
                     if ( fet ) {
-                        p = fisher_test( ldm.at(0).at(0), ldm.at(0).at(1), ldm.at(1).at(0), ldm.at(1).at(1) ) ;
+                        double q = kt_fisher_exact( ldm.at(0).at(0), ldm.at(0).at(1), ldm.at(1).at(0), ldm.at(1).at(1), left, right, p ) ;
                     }
                     
                     for ( int i = 0 ; i < ldm.size() ; i ++ ) {
@@ -609,9 +674,9 @@ void tract_info::compute_interparental_ld ( double window_size, bool fet ) {
                         ld[sample.at(i).chromosomes.at(c1->first*2).at(iterator1.at(i)).type][sample.at(i).chromosomes.at(c2->first*2+1).at(iterator2.at(i)).type] ++ ;
                     }
                     
-                    double p = -1 ;
+                    double p, right, left ;
                     if ( fet ) {
-                        p = fisher_test( ld.at(0).at(0), ld.at(0).at(1), ld.at(1).at(0), ld.at(1).at(1) ) ;
+                        double q = kt_fisher_exact( ld.at(0).at(0), ld.at(0).at(1), ld.at(1).at(0), ld.at(1).at(1), left, right, p ) ;
                     }
                     
                     for ( int i = 0 ; i < ld.size() ; i ++ ) {
